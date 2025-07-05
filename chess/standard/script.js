459fda5a1b9b6b9b067b63091c24288881a91af2
SebastienPaviot <43282316+SebastienPaviot@users.noreply.github.com>
2025-07-05 07:48:24 +0200

    Update script.js

--- chess/standard/script.js
+++ chess/standard/script.js
@@ -4,13 +4,29 @@ const board = Chessboard('board', {
   draggable: true,
   position: 'start',
   pieceTheme: 'https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png',
-  onDrop: function (source, target) {
-    const move = game.move({
-      from: source,
-      to: target,
-      promotion: 'q'
-    });
-
-    if (move === null) return 'snapback';
-  }
+  onDrop: onDrop
 });
+
+function onDrop(source, target) {
+  const move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  });
+
+  // Coup invalide => annuler
+  if (move === null) return 'snapback';
+
+  // Coup valide => attendre un peu et jouer les noirs
+  window.setTimeout(makeRandomMove, 250);
+}
+
+function makeRandomMove() {
+  if (game.game_over()) return;
+
+  const moves = game.moves();
+  const move = moves[Math.floor(Math.random() * moves.length)];
+
+  game.move(move);
+  board.position(game.fen());
+}
